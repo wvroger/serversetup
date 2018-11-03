0e056357e9f94c5b42b1cd5dcb1376ec45872a4d
wvroger <36781386+wvroger@users.noreply.github.com>
2018-11-02 20:15:29 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-
+##Test Roger
 setenforce 0
 sed -i 's/enforcing/disabled/g' /etc/selinux/config /etc/selinux/config
 rpm --import "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF"

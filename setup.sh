7984dd7c681a7e3094965eef7c379232e4314c76
wvroger <36781386+wvroger@users.noreply.github.com>
2018-11-02 20:29:32 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ yum remove firewalld -y
 yum install mono-core mono-devel mono-locale-extras curl mediainfo -y
 yum install -y glusterfs-server mono-devel
 yum -y install https://www.rpmfind.net/linux/rpmfusion/nonfree/el/updates/7/x86_64/u/unrar-5.4.5-1.el7.x86_64.rpm   # Install unrar
-yum install wget mediainfo libzen libmediainfo curl gettext mono-core mono-devel sqlite.x86_64 -y
+yum install -y wget mediainfo libzen libmediainfo curl gettext mono-core mono-devel sqlite.x86_64 
 yum -y install wget git par2cmdline p7zip unzip tar gcc python-feedparser python-configobj python-cheetah python-dbus python-devel libxslt-devel
 yum-config-manager --enable remi-php72-php
 yum install -y php php-mcrypt php-cli php-gd php-curl php-mysql php-ldap php-zip php-fileinfo 
@@ -19,19 +19,19 @@ yum install -y memcached p7zip php72-php-mbstring php72-php-mcrypt php72-php-pec
 yum install -y php72-php-fpm-nginx.noarch php72-php-opcache.x86_64 php72-php-pear.noarch php72-php-pdo.x86_64 php72-php-gd.x86_64 
 yum install -y php72-php-mysqlnd.x86_64 php72 php72-php-common php72-common zlib-devel bzip2-devel openssl-devel ncurses-devel 
 yum install -y sqlite-devel readline-devel tk-devel nasm -y
-
-
-
+#
+#
+#
 #service glusterd start && chkconfig glusterd on
-
- 
+#
+#
 yum -y install ntp 
 ntpdate pool.ntp.org 
 service ntpd start 
 chkconfig ntpd on
 ln -s /usr/lib64/libMonoPosixHelper.so /usr/lib/libMonoPosixHelper.so
 rpm -ivh http://download.neorouter.com/Downloads/NRMesh/Update_2.4.5.4510/Linux/CentOS/nrclient-2.4.5.4510-mesh-centos-x86_64.rpm
-
-
-
+#
+#
+#a
 yum -y update && reboot
